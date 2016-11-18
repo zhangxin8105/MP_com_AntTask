b0687321e6c85b36a9ce1d50f5769fca8a6e368f
zhangxin <zhangxin8105@gmail.com>
2016-11-18 10:33:51 +0800

    上传基本框架

---
+++ AntTask/src/zhangxin/ant/task/ZhxInput.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2016 - Zhang Xin
+ *
+ * Date: 2016年11月18日  上午10:28:24
+ */
+
+package zhangxin.ant.task;
+
+/**
+ * @author Zhang Xin
+ *
+ */
+public class ZhxInput extends org.apache.tools.ant.Task {
+
+}
